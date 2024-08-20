bfd0f631a941b4c24ef483fd3a50736ec9ae8158
Leandro Rey <leandrodrey@gmail.com>
2024-08-20 15:47:23 -0300

    Add initial setup for Next.js app with TailwindCSS and ESLint
    Created necessary configuration files for a new Next.js project including `package.json`, `.gitignore`, and TailwindCSS setup. Added ESLint configuration with Next.js core web vitals extension. Included favicon, global CSS, and basic layout setup.

---
+++ app/page.js
@@ -0,0 +1,75 @@
+'use client'
+import {useEffect, useState} from 'react';
+import Link from "next/link";
+
+export default function Home() {
+
+    const [recipes, setRecipes] = useState([]);
+    const [searchTerm, setSearchTerm] = useState("");
+
+    const handleSearch = (e) => {
+        e.preventDefault();
+        setSearchTerm(e.target.elements.search.value);
+    }
+
+    const fetchRecipes = async (url) => {
+        try {
+            const response = await fetch(url);
+            if (!response.ok) {
+                throw new Error('Error al obtener las recetas');
+            }
+            const data = await response.json();
+            setRecipes(data);
+        } catch (error) {
+            console.error("Error fetching recipes:", error);
+        }
+    };
+
+    useEffect(() => {
+        let url = 'http://localhost:4000/recipes';
+        if (searchTerm) {
+            url = `http://localhost:4000/recipes/${searchTerm}`;
+        }
+        fetchRecipes(url);
+    }, [searchTerm]);
+
+    return (
+        <>
+            <h1 className="text-2xl mb-2">Food Recipes</h1>
+            <form onSubmit={handleSearch} className="flex flex-row items-center justify-center">
+                <input
+                    type="text"
+                    id="search"
+                    name="search"
+                    className="border-2 px-1 py-1 text-black"
+                />
+                <button type="submit" className="px-1 py-1 border-2">
+                    Search
+                </button>
+            </form>
+
+            <a href="/recipes" className="block underline py-2">Ver valoraciones</a>
+
+            <table className="table-auto">
+                <thead>
+                <tr>
+                    <th className="px-4 py-2">Nombre</th>
+                    <th className="px-4 py-2">Valoración</th>
+                </tr>
+                </thead>
+                <tbody>
+                {recipes.map((recipe) => (
+                    <tr key={recipe.id}>
+                        <td className="border px-4 py-2">
+                            <Link className="underline text-blue-300 hover:text-blue-500" href={`detail/${recipe.id}`}>
+                                {recipe.name}
+                            </Link>
+                        </td>
+                        <td className="border px-4 py-2">{recipe.score}</td>
+                    </tr>
+                ))}
+                </tbody>
+            </table>
+        </>
+    );
+}
