0311581a402dafda28a32624dde1ce04e1f08544
Leandro Rey <leandrodrey@gmail.com>
2024-08-30 20:17:23 -0300

    Refactor Home component to use recipesService

--- app/page.js
+++ app/page.js
@@ -1,39 +1,22 @@
-'use client'
-import {useState} from 'react';
 import Link from "next/link";
-import {endpoints} from "@/config/endpoints";
-import useFetch from "@/hooks/useFetch";
+import {recipesService} from "@/services/recipes";
 import SearchBar from "@/components/SearchBar";
 import RecipesTable from "@/components/RecipesTable";
 
-export default function Home() {
+export default async function Home({searchParams}) {
 
-    const [searchTerm, setSearchTerm] = useState("");
-
-    const { data: recipes, isLoading, error } = useFetch(endpoints.recipes.getAll(searchTerm));
-
-    const handleSearch = (searchTerm) => {
-        setSearchTerm(searchTerm);
-    };
-
-    if (isLoading) {
-        return <div>Cargando...</div>;
-    }
-
-    if (error) {
-        return <div>Error: {error.message}</div>;
-    }
+    let recipes = await recipesService.getRecipes({ searchTerm: searchParams.search });
 
     if (!recipes) {
-        return <div>Recetas no encontrada</div>;
+        return <div>Recetas no encontradas</div>;
     }
 
     return (
         <>
             <h1 className="text-2xl mb-2">Food Recipes</h1>
-            <SearchBar onSearch={handleSearch} />
+            <SearchBar />
             <Link href="/ratings" className="block underline py-2">Ver valoraciones</Link>
-            <RecipesTable recipes={recipes} />
+            <RecipesTable recipes={recipes}/>
         </>
     );
 }
